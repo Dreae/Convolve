cc7314f2b9c8b4b65631ed9246adb54b5439edfb
Dreae <penitenttangentt@gmail.com>
2014-11-08 22:34:00 -0500

    More operators, multiplication

--- src/main/scala/dreae/convolve/Matrix.scala
+++ src/main/scala/dreae/convolve/Matrix.scala
@@ -1,29 +1,62 @@
 package dreae.convolve
 
-import dreae.convolve.Operator.OpAdd
+import Operator._
 
 class Matrix[T](val matr: Seq[Seq[T]]) {
   val rows = matr.size
   val cols = matr.head.size
 
-  def this(array: List[T], cols: Int) = this{
-    val rows = {
-      for(i <- 0 until (array.size / cols)) yield {
-        array.slice(i * cols, (i * cols) + cols)
-      }
-    }.toList
-    rows
-  }
+  def apply(row: Int)(col: Int) = matr(row)(col)
 
-  def apply(col: Int)(row: Int) = matr(col)(row)
-
-  def +[B](other: Matrix[B])(implicit add: Operator[T,B,OpAdd,_]) = {
-    if(other.rows != rows || other.cols != cols) {
-      throw new Exception()
-    }
+  def +[B,R](other: Matrix[B])(implicit add: Operator[T,B,OpAdd,R]) = {
+    if(other.rows != rows || other.cols != cols) throw new Exception()
     val newMatr = for(i <- 0 until matr.size) yield {
       for(j <- 0 until matr(i).size) yield add(this(i)(j), other(i)(j))
     }
     new Matrix(newMatr)
+  }
+
+  def -[B,R](other: Matrix[B])(implicit sub: Operator[T,B,OpSub,R]) = {
+    if(other.rows != rows || other.cols != cols) throw new Exception()
+    val newMatr = for(i <- 0 until matr.size) yield {
+      for(j <- 0 until matr(i).size) yield sub(this(i)(j), other(i)(j))
+    }
+    new Matrix(newMatr)
+  }
+
+  def *[B,R,F](other: Matrix[B])(implicit mul: Operator[T,B,OpMul,R], add: Operator[R,R,OpAdd,R]) = {
+    if(cols != other.rows) throw new Exception()
+    val newMatr = for(i <- 0 until rows) yield {
+      for(p <- 0 until other.cols) yield {
+        val newCol = for(m <- 0 until cols) yield {
+          mul(this(i)(m), other(m)(p))
+        }
+        newCol.tail.foldLeft(newCol.head)(add(_,_))
+      }
+    }
+    new Matrix(newMatr)
+  }
+
+  def *[B,R](c: B)(implicit mul: Operator[T,B,OpMul,R]) = {
+    val newMatr = for(row <- matr) yield {
+      for(e <- row) yield mul(e, c)
+    }
+    new Matrix(newMatr)
+  }
+
+  lazy val T = {
+    val newMatr = for(i <- 0 until cols) yield {
+      for(j <- 0 until rows) yield this(j)(i)
+    }
+    new Matrix(newMatr)
+  }
+
+  override lazy val toString = {
+    val str = new StringBuilder()
+    for (i <- 0 until rows) {
+      for(j <- 0 until cols) str.append(this(i)(j) + " ")
+      str.append('\n')
+    }
+    str.mkString
   }
 }
--- src/main/scala/dreae/convolve/Operator.scala
+++ src/main/scala/dreae/convolve/Operator.scala
@@ -8,11 +8,65 @@ object Operator {
   trait OpType
   trait OpAdd extends OpType
   trait OpSub extends OpType
+  trait OpMul extends OpType
 
   implicit object OpAddII extends Operator[Int, Int, OpAdd, Int] {
     override def apply(a: Int, b: Int) = a + b
   }
+  implicit object OpSubII extends Operator[Int, Int, OpSub, Int] {
+    override def apply(a: Int, b: Int) = a - b
+  }
+  implicit object OpMulII extends Operator[Int, Int, OpMul, Int] {
+    override def apply(a: Int, b: Int) = a * b
+  }
+
+  implicit object OpAddLL extends Operator[Long, Long, OpAdd, Long] {
+    override def apply(a: Long, b: Long) = a + b
+  }
+  implicit object OpSubLL extends Operator[Long, Long, OpSub, Long] {
+    override def apply(a: Long, b: Long) = a - b
+  }
+  implicit object OpMulLL extends Operator[Long, Long, OpMul, Long] {
+    override def apply(a: Long, b: Long) = a * b
+  }
+
+  implicit object OpAddFF extends Operator[Float, Float, OpAdd, Float] {
+    override def apply(a: Float, b: Float) = a + b
+  }
+  implicit object OpSubFF extends Operator[Float, Float, OpSub, Float] {
+    override def apply(a: Float, b: Float) = a - b
+  }
+  implicit object OpMulFF extends Operator[Float, Float, OpMul, Float] {
+    override def apply(a: Float, b: Float) = a * b
+  }
+
+  implicit object OpAddDD extends Operator[Double, Double, OpAdd, Double] {
+    override def apply(a: Double, b: Double) = a + b
+  }
+  implicit object OpSubDD extends Operator[Double, Double, OpSub, Double] {
+    override def apply(a: Double, b: Double) = a - b
+  }
+  implicit object OpMulDD extends Operator[Double, Double, OpMul, Double] {
+    override def apply(a: Double, b: Double) = a * b
+  }
+
   implicit object OpAddIL extends Operator[Int, Long, OpAdd, Long] {
     override def apply(a: Int, b: Long) = a + b
+  }
+  implicit object OpSubIL extends Operator[Int, Long, OpSub, Long] {
+    override def apply(a: Int, b: Long) = a - b
+  }
+  implicit object OpMulIL extends Operator[Int, Long, OpMul, Long] {
+    override def apply(a: Int, b: Long) = a * b
+  }
+
+  implicit object OpAddLI extends Operator[Long, Int, OpMul, Long] {
+    override def apply(a: Long, b: Int) = a + b
+  }
+  implicit object OpSubLI extends Operator[Long, Int, OpMul, Long] {
+    override def apply(a: Long, b: Int) = a - b
+  }
+  implicit object OpMulLI extends Operator[Long, Int, OpMul, Long] {
+    override def apply(a: Long, b: Int) = a * b
   }
 }
